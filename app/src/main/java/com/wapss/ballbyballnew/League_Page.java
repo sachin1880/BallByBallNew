0c6876c727c12718c8186dbc0329d4bb93ea4199
wass-mac <palashnayek111@gmail.com>
2024-03-18 12:56:11 +0530

    wallet and league page design

---
+++ app/src/main/java/com/wapss/ballbyballnew/League_Page.java
@@ -0,0 +1,14 @@
+package com.wapss.ballbyballnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class League_Page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_league_page);
+    }
+}
