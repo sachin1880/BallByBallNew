069fbf9a7f681af2e5d63562236d6221c40cf79e
Sachin <sachinsingh1880@gmail.com>
2024-03-18 12:57:17 +0530

    working design

---
+++ app/src/main/java/com/wapss/ballbyballnew/apiService/ApiHolder.java
@@ -0,0 +1,14 @@
+package com.wapss.ballbyballnew.apiService;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface ApiHolder {
+//    @POST("login")
+//    @FormUrlEncoded
+//    Call<LoginResponse> login(@Field("email") String email,
+//                              @Field("password") String passWord,
+//                              @Field("fcm") String fcm);
+}
